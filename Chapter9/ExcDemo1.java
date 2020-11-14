15af8a8a44bcc5b89188f70d2f9f986bc2751173
Бекзадбек Насирахунов <n.bekzad@gmail.com>
2020-11-14 13:52:07 +0600

    Started Chapter 9

---
+++ Chapter9/ExcDemo1.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class ExcDemo1 {
+    public static void main(String args[]) {
+        int[] nums = new int[4];
+
+        try {
+            nums[4] = 5;
+            System.out.println("This won't be displayed");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+            System.out.println("Array out of bounds");
+
+        }
+        System.out.println("After the catch statement");
+    }
+}
